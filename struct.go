88a4ef5ccf462e8d978bb7d1f5dc20336f158d41
Manish Panwar <manish.panwar40@gmail.com>
2023-10-17 14:23:05 +0400

    Revert "struct old"
    This reverts commit 3acdf7edb33da6a7d41901938452c5ab28a4478e.

--- struct.go
+++ struct.go
@@ -1,24 +1,33 @@
-// struct is like array, but can keep values of different data types
+//struct is like array, but can keep values of different data types
 package main
 
-type Person struct {
-	name         string
-	age          int
-	salary       int
-	organization string
+import (
+	"fmt"
+)
+
+type Person struct{
+	name string
+	age int
+	salary int
+	organization
 }
 
-func main() {
+func main(){
 	var pers1 Person
 	var pers2 Person
 
 	pers1.name = "Manish"
-	pers1.age = 30
+	pers1. age = 30
 	pers1.salary = 10
 	pers1.organization = "ABC"
 
-	pers1.name = "Panwar"
-	pers1.age = 20
-	pers1.salary = 15
-	pers1.organization = "ABCD"
+	fmt.Println(pers1.name, pers1.age, pers1.salary, pers1.organization)
+
+	pers2.name = "Panwar"
+	pers2.age = 20
+	pers2.salary = 15
+	pers2.organization = "ABCD"
+
+	fmt.Println(pers2.name, pers2.age, pers2.salary, pers2.organization)
+
 }
