76905771a2943a6bc2f6661546794be6aadcd045
Manish Panwar <manish.panwar40@gmail.com>
2023-10-17 14:00:08 +0400

    struct

---
+++ struct.go
@@ -0,0 +1,27 @@
+//struct is like array, but can keep values of different data types
+package main
+
+import ("fmt")
+
+type Person struct{
+	name string
+	age int
+	salary int
+	organization
+}
+
+func main(){
+	var pers1 Person
+	var pers2 Person
+
+	pers1.name = "Manish"
+	pers1. age = 30
+	pers1.salary = 10
+	pers1.organization = "ABC"
+
+	
+	pers1.name = "Panwar"
+	pers1. age = 20
+	pers1.salary = 15
+	pers1.organization = "ABCD"
+}
